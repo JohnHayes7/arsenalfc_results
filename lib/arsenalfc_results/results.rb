e33258b17d044d54b618530aaa07b65bfac883a1
John Hayes <johnnyhayes7@gmail.com>
2019-03-08 20:41:22 +0000

    reorganizing structure

--- lib/arsenalfc_results/results.rb
+++ lib/arsenalfc_results/results.rb
@@ -5,7 +5,7 @@ class ArsenalfcResults::Results
   def greeting
     puts "Greetings! Welcome to Arsenal FC Results"
     puts "Please select a season:"
-    Seasons.years.each_with_index do |s, i|
+    Seasons.seasons.each_with_index do |s, i|
       puts "#{i+1}. #{s}"
     end
   end
--- lib/arsenalfc_results/seasons.rb
+++ lib/arsenalfc_results/seasons.rb
@@ -5,8 +5,9 @@ require "pry"
 
 
 class Seasons
+  attr_accessor :season, :competition
   
-  @@years = []
+  @@all_seasons = []
   season_19 = "https://www.11v11.com/teams/arsenal/tab/matches/season/2019/"
   season_18 = "https://www.11v11.com/teams/arsenal/tab/matches/season/2018/"
   
@@ -16,19 +17,19 @@ class Seasons
     @@season_array.each do |s|
       site = s
       page = Nokogiri::HTML(open(site))
-      @@years << page.css("h2.seasonTitle").text.split(" ")[0]
+      @@all_seasons << page.css("h2.seasonTitle").text.split(" ")[0]
       # binding.pry
     end
   end
   self.season_scraper
   
-  def self.years
-    @@years
+  def self.seasons
+    @@all_seasons
   end
   
-  def self.seasons 
-    @@season_array
-  end
+  # def self.seasons 
+  #   @@season_array
+  # end
   
   
 end
