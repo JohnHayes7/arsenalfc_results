8624a376f035503ce940cb4159c01a559004c273
John Hayes <johnnyhayes7@gmail.com>
2019-03-11 12:00:03 +0000

    working on competition iterator

--- lib/competition.rb
+++ lib/competition.rb
@@ -2,15 +2,24 @@ require_relative "arsenalfc_results"
 
 class ArsenalfcResults::Competition
   
+  attr_accessor :name
+  
   def self.competition_scraper
+    @comp_array = []
     site = "https://www.11v11.com/teams/arsenal/tab/matches/"
     page = Nokogiri::HTML(open(site))
-    comp = page.css("table.width580 tr td").drop(4)
-    comp.select_with_index
-    binding.pry
+    drop_four = page.css("table.width580 tr td").drop(4)
+    comps = drop_four.each_with_index.select{|x, i| i % 5 == 0}
+    comps.each do |c|
+      c.each_with_index do |cmp, i|
+        i % 2 == 0
+        @comp_array << cmp
+        binding.pry
+      end
+    end
   end
    
-   self.competition_scraper
+  self.competition_scraper
   
   # def self.all_competitions 
   #   @@comp_array
